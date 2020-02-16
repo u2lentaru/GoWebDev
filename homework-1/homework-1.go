e96b6647ef6a0ee81012abad8fda8230bbd74b17
u2 <u2@lenta.ru>
2020-02-16 15:28:13 +0500

    homework-1.go hbm

--- homework-1/homework-1.go
+++ homework-1/homework-1.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	resp, err := http.Get("https://golang.org")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Println(resp.Status)
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header)
+	io.Copy(os.Stdout, resp.Body)
 }

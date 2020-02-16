cedf0821c0f0383a89ea5bb18e496853afd7669b
u2 <u2@lenta.ru>
2020-02-16 21:56:57 +0500

    homework-1.go hbm

--- homework-1/homework-1.go
+++ homework-1/homework-1.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
+	"log"
 	"os"
-	"strings"
+	"os/signal"
+
+	gowebsocket "github.com/sacOO7/GoWebsocket"
 )
 
 func main() {
@@ -21,7 +21,7 @@ func main() {
 	io.Copy(os.Stdout, resp.Body)
 	*/
 
-	resp, err := http.Post(
+	/*resp, err := http.Post(
 		"https://postman-echo.com/post",
 		"application/json",
 		strings.NewReader(`{"foo1":"bar1","foo2":"bar2"}`),
@@ -32,4 +32,32 @@ func main() {
 	}
 	defer resp.Body.Close()
 	io.Copy(os.Stdout, resp.Body)
+	*/
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	socket := gowebsocket.New("ws://echo.websocket.org/")
+	socket.OnConnected = func(socket gowebsocket.Socket) {
+		log.Println("Соединение установлено")
+	}
+
+	socket.OnConnectError = func(err error, socket gowebsocket.Socket) {
+		log.Println("Соединение не установлено", err)
+	}
+
+	socket.OnPingReceived = func(data string, socket gowebsocket.Socket) {
+		log.Println("Получен пинг" + data)
+	}
+
+	socket.Connect()
+
+	for {
+		select {
+		case <-interrupt:
+			log.Println("Закрытие соединения с сервером")
+			socket.Close()
+			return
+		}
+	}
 }

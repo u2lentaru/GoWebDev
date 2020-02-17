1aa052cdd72abcdec9d6752151759da3624a8515
u2 <u2@lenta.ru>
2020-02-17 22:46:13 +0500

    getYandexFile added

--- homework-1/homework-1.go
+++ homework-1/homework-1.go
@@ -4,36 +4,40 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strings"
 )
 
 func main() {
-	urls := []string{"https://tass.ru", "https://rbc.ru", "https://ria.ru"}
-	strSearch(os.Args[1], urls)
-	// fmt.Println("Pages contains '" + os.Args[1] + "':")
+
+	//urls := []string{"https://tass.ru", "https://rbc.ru", "https://ria.ru"}
 
 	// for _, resurl := range strSearch(os.Args[1], urls) {
 	// 	fmt.Println(resurl)
 	// }
 
+	pk := "https://yadi.sk/i/03bE933n3PqpG2"
+	yaurl := "https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key=" + pk
+	//yaurl := "https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key=https://yadi.sk/i/03bE933n3PqpG2"
+	getYandexFile(yaurl)
+
 }
 
-func strSearch(str string, urls []string) {
+func strSearch(str string, urls []string) []string {
+
 	resultUrls := []string{}
 
 	for _, url := range urls {
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Println(err)
-			return
+			return resultUrls
 		}
 		fmt.Println(url)
 		fmt.Println(resp.Status)
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			fmt.Println(err)
-			return
+			return resultUrls
 		}
 		fmt.Println(strings.Count(string(body), str))
 		if strings.Contains(string(body), str) {
@@ -43,10 +47,19 @@ func strSearch(str string, urls []string) {
 
 	fmt.Println("Pages contains '" + str + "':")
 
-	for _, resurl := range resultUrls {
-		fmt.Println(resurl)
+	return resultUrls
+}
+
+func getYandexFile(yaurl string) {
+	fmt.Println(yaurl)
+	resp, err := http.Get(yaurl)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	//return
+
+	fmt.Println(resp.Body)
+
 }
 
 // PS C:\Golang_work\src\GoWebDev> go run C:\Golang_work\src\GoWebDev\homework-1\homework-1.go нефть

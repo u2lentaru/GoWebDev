422b51bd3012decad4726a5b1a786598f5d17d4a
u2 <u2@lenta.ru>
2020-02-19 21:42:19 +0500

    firstHandle, helloUsername added

--- homework-2/homework-2.go
+++ homework-2/homework-2.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", firstHandle)
+	router.HandleFunc("/user", helloUsername)
+
+	log.Fatal(http.ListenAndServe("8080", router))
+
+}
+
+func firstHandle(wr http.ResponseWriter, req *http.Request) {
+	wr.Write([]byte("Hello world!"))
+}
+
+func helloUsername(wr http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(wr, "Hello %s!", req.URL.Query().Get("name"))
 }

028900088f040419e1edacc1d2d126cc87d38944
u2 <u2@lenta.ru>
2020-02-20 18:00:43 +0500

    hw-2_client.go hbm

--- homework-2/hw-2_client/hw-2_client.go
+++ homework-2/hw-2_client/hw-2_client.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func main() {
+	type SearchStruct struct {
+		Search string   `json:"search"`
+		Sites  []string `json:"sites"`
+	}
+	searchStruct := SearchStruct{
+		Search: "нефть",
+		Sites: []string{
+			"https://tass.ru",
+			"https://rbc.ru",
+			"https://ria.ru",
+		},
+	}
+
+	sendJSON, err := json.Marshal(searchStruct)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Printf("%s", sendJSON)
+
+	//resp, err := http.Post("localhost:8080","application/json", bytes.NewBuffer(reqBody))
 
 }

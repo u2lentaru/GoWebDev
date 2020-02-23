dc026f2bf844d967f8e714306b523a28ad99b576
u2 <u2@lenta.ru>
2020-02-23 23:00:30 +0500

    structs added

--- homework-3/homework-3.go
+++ homework-3/homework-3.go
@@ -1,5 +1,24 @@
 package main
 
 func main() {
+	type TPost struct {
+		Subj     string
+		PostTime string
+		Text     string
+	}
+
+	type TBlog struct {
+		Title    string
+		PostList []TPost
+	}
+
+	var MyBlog = TBlog{
+		Title: "My blog",
+		PostList: []TPost{
+			TPost{"1st subj", "01.01.2020", "1st text"},
+			TPost{"2nd subj", "02.01.2020", "2nd text"},
+			TPost{"3rd subj", "03.01.2020", "3rd text"},
+		},
+	}
 
 }
